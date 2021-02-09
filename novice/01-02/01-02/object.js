cf8d327b038943839957bb00840d40b1b28f0f82
Ahmad Haunan <akhmad.ha@students.amikom.ac.id>
2021-02-09 13:26:37 +0700

    hari2

--- novice/01-02/01-02/object.js
+++
@@ -1,9 +0,0 @@
-let mahasiswa = {
-    nim : '18.11.2416',
-    nama : 'akhmad haunan',
-};
-
-//console.log(mahasiswa);
-console.log(mahasiswa['nim']);
-console.log(mahasiswa.nama);
-console.log(mahasiswa.mbuh);
