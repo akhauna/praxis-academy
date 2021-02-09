4848e00888d4926bd56ef7bc992bb5a6210cb7c3
Ahmad Haunan <akhmad.ha@students.amikom.ac.id>
2021-02-09 15:52:13 +0700

    hari2

---
+++ novice/01-02/fungsireturn.js
@@ -0,0 +1,19 @@
+
+function kali(a,b){
+    hasil=a*b;
+    return hasil;
+}
+
+//panggil fungsi
+
+var x=2;
+var y=3;
+var hasilkali = kali(x,y);
+console.log(hasilkali);
+
+var nilai1=10;
+var nilai2=20;
+var hasilkali = kali(nilai1,nilai2);
+console.log(hasilkali);
+
+//--------------------------------
---
+++ novice/01-02/perulanganarray.js
@@ -0,0 +1,5 @@
+var nama =["nabil", "uhsin","janna","ajik","baginda","topik","nala"]
+
+for(i in nama){
+    console.log(i+". "+nama[i]);
+}
