f6e9ae8df3e4372a555e9c29aea1ecdefb164558
Ahmad Haunan <akhmad.ha@students.amikom.ac.id>
2021-02-09 14:25:11 +0700

    hari2

---
+++ novice/01-02/fungsi.js
@@ -0,0 +1,32 @@
+/*
+function salam(){
+    console.log("halo ak gembul");
+}
+
+salam()
+
+//------------------------
+function perkalian(a,b){
+    hasilkali= a*b;
+    console.log("hasil kali a dan b="+ hasilkali)
+}
+perkalian(4,5)
+
+//-------------------------
+//a=alas
+//t=tinggi
+function luasSegitiga(a,t){
+    hasil=1/2*a*t;
+    console.log("luas segitiga="+hasil)
+}
+luasSegitiga(2,4)
+
+//-------------------------
+//luaslingkaran= pi x r x r
+function luasLingkaran(r){
+let pi=3.14;
+    hasil=pi*r*r;
+    console.log("luas lingkaran="+hasil)
+}
+luasLingkaran(4)
+*/
