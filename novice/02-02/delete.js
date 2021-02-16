8fb1d8fe1c17141393043299f5bac34072057dd5
Ahmad Haunan <akhmad.ha@students.amikom.ac.id>
2021-02-16 10:23:46 +0700

    hari2

--- novice/02-02/delete.js
+++ novice/02-02/delete.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.unlink('mynewfile2.txt', function (err) {
+fs.unlink('mynewfile1.txt', function (err) {
     if (err) throw err;
     console.log('File deleted!');
 }); 
