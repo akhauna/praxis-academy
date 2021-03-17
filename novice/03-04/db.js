9e54f3aded9b47057c9767293ac93592bb4ecfef
Haunan <akhmad.ha@students.amikom.ac.id>
2021-03-17 20:38:46 +0700

    ntap

--- novice/03-04/db.js
+++ novice/03-04/db.js
@@ -3,7 +3,8 @@ const PgClient = require('pg').Client;
 const db = new PgClient({
     database: 'unanshop',
     user: 'postgres',
-    password: 'unanganteng'
+    password: 'unanganteng',
+    port :'8080'
 });
 
 module.exports = db;
